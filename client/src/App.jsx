0e13b947f90d67f634ab026c6c6fcad68ed04b79
MuhammadSarimWaseem <sarimwaseem84@gmail.com>
2025-04-03 18:01:46 +0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,6 @@ import Cart from "./Pages/CartPage/Cart";
 import Orders from "./Pages/ViewOrdersPage/ViewOrder";
 import AddSeller from "./Pages/AddSellerPage/AddSeller";
 
-
 function App() {
   return (
     <Fragment>
@@ -35,14 +34,14 @@ function App() {
       <Router>
         <Navbar></Navbar>
         <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/addProducts" element={<AddProducts/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/orders" element={<Orders/>}/>
-          <Route path="/addSeller" element={<AddSeller/>}/>
+            <Route path="/*" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/landing" element={<Landing />} />
+            <Route path="/addProducts" element={<AddProducts />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/addSeller" element={<AddSeller />} />
         </Routes>
       </Router>
     </Fragment>
